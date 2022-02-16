4680cd2b58c388fa1af34bbfe6b9b4c75de70435
eliasjr <elias.dvlpr@gmail.com>
2022-02-15 21:27:16 -0300

    conexao com banco e flyway

--- src/main/kotlin/br/com/eliasjr/mercadolivro/model/CustomerModel.kt
+++ src/main/kotlin/br/com/eliasjr/mercadolivro/model/CustomerModel.kt
@@ -1,7 +1,17 @@
 package br.com.eliasjr.mercadolivro.model
 
+import javax.persistence.*
+
+@Entity(name = "customer")
 data class CustomerModel (
-    var id: String? = null,
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Int? = null,
+
+    @Column
     var name: String,
+
+    @Column
     var email: String
 )
---
+++ src/main/kotlin/br/com/eliasjr/mercadolivro/repository/CustomerRepository.kt
@@ -0,0 +1,9 @@
+package br.com.eliasjr.mercadolivro.repository
+
+import br.com.eliasjr.mercadolivro.model.CustomerModel
+import org.springframework.data.repository.CrudRepository
+
+interface CustomerRepository : CrudRepository<CustomerModel, Int> {
+
+    fun findByNameContaining(name: String) : List<CustomerModel>
+}
